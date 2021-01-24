420acf8f94ebb7ad687a04afb59f631d15715f27
Chimera <j.calderon3@gmail.com>
2021-01-24 16:48:18 -0500

    connection script updated

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,22 @@
 // setup to connect Node to MySQL
+const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    // database: "(burger_db)?"
+});
 
-// export the connection
+// Make connection
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
+// export connection for ORM use
+module.exports = connection;

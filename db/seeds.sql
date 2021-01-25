8371491554c6ec06dbd54022a215cae216b38e32
Chimera <j.calderon3@gmail.com>
2021-01-25 18:42:15 -0500

    connection script updated; db directory updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- insert queries with at least three entries
+
+INSERT INTO burgers (name) VALUES ('PB AND Jae');
+INSERT INTO burgers (name) VALUES ('Grilled Cheezus');
+INSERT INTO burgers (name, devoured) VALUES ('Smores', true);
---
+++ models/burger.js
@@ -0,0 +1,9 @@
+// Import the ORM to create function that will interact with the database
+const orm = require("../config/orm.js");
+
+const burger = {
+
+};
+
+// Export the database functions for the controller (burger_controller.js)
+module.exports = burger;

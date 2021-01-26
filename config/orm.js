2b848fb01aa1b842b78aebbbdcfe51245b6bb091
Chimera <j.calderon3@gmail.com>
2021-01-25 19:16:02 -0500

    orm and controller script updated

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,16 @@
 // Import MySQL connection
 const connection = require("../config/connection.js");
 
+const orm = {
+    selectWhere: function(tableInput, colToSearch, valOfCol, callback) {
+        const queryString = "SELECT * FROM ?? WHERE ?? = ?";
 
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+            if (err) throw err;
+            callback(result);
+        });
+    }
+};
 
 // Export the orm object for the model (burger.js)
 module.exports = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (burger.js) to use its database functions
+const burger = require("../models/burger.js");
+
+

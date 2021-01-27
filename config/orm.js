b338eb894c82481e47545b23a6ed708f513fb443
Chimera <j.calderon3@gmail.com>
2021-01-27 17:32:57 -0500

    ORM script updated

--- config/orm.js
+++ config/orm.js
@@ -2,12 +2,61 @@
 const connection = require("../config/connection.js");
 
 const orm = {
-    selectWhere: function(tableInput, colToSearch, valOfCol, callback) {
-        const queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    all: function(tableInput, callback) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
 
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+        connection.query(queryString, function(err, result) {
             if (err) throw err;
             callback(result);
+        });
+    },
+    create: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+      }
+
+            cb(result);
+        });
+    },
+    delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
         });
     }
 };
